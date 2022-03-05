b7e3a824bfa17d5ec9ac24dcb7c0d13c4de76525
SoNgSeUNgMiN96 <tmd9544@gmail.com>
2022-03-06 04:15:38 +0900

    todo-app 초기버전 완성, 컴포넌트 성능 최적화 이전
    작성일 : 2022-03-26

--- src/App.js
+++ src/App.js
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, { useCallback, useState, useRef } from 'react';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
-  return <div>Todo App을 만들자!</div>;
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링 보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos], //todos가 바뀌었을 때만 함수 생성.
+  );
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id)); //todo를 돌리며 todoid에 해당하는애들 지운다.
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+
+  return (
+    <TodoTemplate>
+      <TodoInsert
+        onInsert={onInsert} //props로  onInsert함수를 넘겨줘서, TodoInsert에서 해당 함수를 사용해 값을 추가한다.
+      />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+    </TodoTemplate>
+  );
 };
 
 export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import TodoListItem from './TodoListItem';
+import './TodoList.scss';
+
+const TodoList = ({ todos, onRemove, onToggle }) => {
+  return (
+    <div className="TodoList">
+      {
+        //todos 배열을 받아와서 map을 통해 각 요소를 돌린다.
+      }
+      {todos.map((todo) => (
+        <TodoListItem
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
